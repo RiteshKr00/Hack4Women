4eb7a9e69d10f6a63dfe28ddd1ebf1d84464624e
slowgeek <kumarritesh14062000@gmail.com>
2021-12-17 14:48:49 +0530

    Chat Setup

--- server/app.js
+++ server/app.js
@@ -2,8 +2,20 @@ const express = require("express");
 const app = express();
 const socket = require("socket.io");
 app.use(express.json()); //repalcement of bodyparser
-
+require("dotenv").config();
 
 const server = app.listen(process.env.PORT, () => {
-    console.log("Server is runnng at port", process.env.PORT);
+  console.log("Server is runnng at port", process.env.PORT);
+});
+
+const io = socket(server);
+io.on("connection", (socket) => {
+  console.log(`User Connected: ${socket.id}`);
+
+  socket.on("disconnect", () => {
+    
+    console.log("User Disconnected", socket.id);
   });
+
+});
+
